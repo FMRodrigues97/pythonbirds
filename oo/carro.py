b5f5dd0b843d5a04a702d1d55b1f68e12a34d418
FMRodrigues97 <fernandodemoraes13@gmail.com>
2020-04-04 19:09:15 -0300

    Implementado Carro

--- oo/carro.py
+++ oo/carro.py
@@ -91,12 +91,36 @@ A Direção terá a responsabilidade de controlar a direção. Ela oferece os se
     'Oeste'
 """
 
+class Carro:
+    def __init__(self, direcao, motor):
+        self.direcao = direcao
+        self.motor = motor
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        return self.motor.acelerar()
+
+    def frear(self):
+        return self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        return self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        return self.direcao.girar_a_esquerda()
+
+
 NORTE='Norte'
 SUL='Sul'
 LESTE='Leste'
 OESTE='Oeste'
 
-class Direcao
+class Direcao:
     rotacao_a_direita_dct = {NORTE:LESTE, LESTE:SUL, SUL:OESTE, OESTE:NORTE}
     rotacao_a_esquerda_dct = {NORTE:OESTE, OESTE:SUL, SUL:LESTE, LESTE:NORTE}
 
@@ -105,6 +129,7 @@ class Direcao
 
     def girar_a_direita(self):
         self.valor=self.rotacao_a_direita_dct[self.valor]
+
     def girar_a_esquerda(self):
         self.valor=self.rotacao_a_esquerda_dct[self.valor]
 
