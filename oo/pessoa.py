420e582914b625257e74983b0664ac7e08fcda1e
FMRodrigues97 <fernandodemoraes13@gmail.com>
2020-04-04 18:40:19 -0300

    Implementados Motor e Direcao

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -9,6 +9,14 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
 if __name__ == '__main__':
     fernando = Pessoa(nome='Fernando')
     alessandra = Pessoa(fernando, nome='Alessandra')
@@ -30,3 +38,5 @@ if __name__ == '__main__':
     print(fernando.olhos)
     print(alessandra.olhos)
     print(id(Pessoa.olhos), id(fernando.olhos), id(alessandra.olhos))
+    print(Pessoa.metodo_estatico(), alessandra.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), alessandra.nome_e_atributos_de_classe())
